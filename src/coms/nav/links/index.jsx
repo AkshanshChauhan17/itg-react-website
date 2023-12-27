109f242b7a2448f9f03c6cb617c4fb9df61d7ef4
AkshanshChauhan17 <akshanshchauhan47@gmail.com>
2023-12-27 14:24:36 +0530

    changes in bottom of the nav bar

--- src/coms/nav/links/index.jsx
+++ src/coms/nav/links/index.jsx
@@ -5,13 +5,13 @@ export default function NavLinks() {
     return (
         <div className="nav-links">
             <div className="upper">
-                <p className="fs-mid bx" onClick={(e)=>setNavLink(e.target.innerText)}>HOME</p>
-                <p className="fs-mid bx" onClick={(e)=>setNavLink(e.target.innerText)}>ABOUT US</p>
-                <p className="fs-mid bx" onClick={(e)=>setNavLink(e.target.innerText)}>ADMINISTRATION</p>
-                <p className="fs-mid bx" onClick={(e)=>setNavLink(e.target.innerText)}>ACADEMIC</p>
-                <p className="fs-mid bx" onClick={(e)=>setNavLink(e.target.innerText)}>DEPARTMENT</p>
-                <p className="fs-mid bx" onClick={(e)=>setNavLink(e.target.innerText)}>STUDENTS</p>
-                <p className="fs-mid bx" onClick={(e)=>setNavLink(e.target.innerText)}>FACULTIES</p>
+                <p className={navLink==="HOME" ? "fs-mid bx active" : "fs-mid bx unactive"} onClick={(e)=>setNavLink(e.target.innerText)}>HOME</p>
+                <p className={navLink==="ABOUT US" ? "fs-mid bx active" : "fs-mid bx unactive"} onClick={(e)=>setNavLink(e.target.innerText)}>ABOUT US</p>
+                <p className={navLink==="ADMINISTRATION" ? "fs-mid bx active" : "fs-mid bx unactive"} onClick={(e)=>setNavLink(e.target.innerText)}>ADMINISTRATION</p>
+                <p className={navLink==="ACADEMIC" ? "fs-mid bx active" : "fs-mid bx unactive"} onClick={(e)=>setNavLink(e.target.innerText)}>ACADEMIC</p>
+                <p className={navLink==="DEPARTMENT" ? "fs-mid bx active" : "fs-mid bx unactive"} onClick={(e)=>setNavLink(e.target.innerText)}>DEPARTMENT</p>
+                <p className={navLink==="STUDENTS" ? "fs-mid bx active" : "fs-mid bx unactive"} onClick={(e)=>setNavLink(e.target.innerText)}>STUDENTS</p>
+                <p className={navLink==="FACULTIES" ? "fs-mid bx active" : "fs-mid bx unactive"} onClick={(e)=>setNavLink(e.target.innerText)}>FACULTIES</p>
             </div>
             <hr />
             <p className="lower fs-small">/{navLink}</p>
