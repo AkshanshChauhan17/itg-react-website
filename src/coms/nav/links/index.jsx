3daf1c58202f8d9bf5b823632fee150b538132c6
AkshanshChauhan17 <akshanshchauhan47@gmail.com>
2023-12-30 01:51:23 +0530

    added navigation bar auto hide after 5 seconds

--- src/coms/nav/links/index.jsx
+++ src/coms/nav/links/index.jsx
@@ -1,15 +1,27 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { IoIosArrowDropup, IoIosArrowDropdown } from "react-icons/io";
 
 export default function NavLinks({sv}) {
+    const [navBarFocus, setNavBarFocus] = useState(true);
     const [navLink, setNavLink] = useState("");
     const [navClosed, setNavClosed] = useState(false);
+
+    useEffect(()=>{
+        const st_one = setTimeout(() => {
+            setNavBarFocus(false);
+        }, 5000);
+        console.log('clsd')
+        return ()=>{
+            clearTimeout(st_one)
+        }
+    }, [navBarFocus])
+
     return (
-        <div className="nav-links" style={navClosed ? {padding: "10px 80px"}:null}>
+        <div className="nav-links" style={navBarFocus ? {} : {opacity: 0.1, padding: "10px 80px"}} onMouseOver={()=>setNavBarFocus(true)}>
             {
                 navClosed ? <IoIosArrowDropdown className="close" onClick={()=>setNavClosed(false)}/> : <IoIosArrowDropup className="close" onClick={()=>setNavClosed(true)}/>
             }
-            <div className="upper" style={navClosed ? {display: "none", padding: 0} : null}>
+            <div className="upper" style={navClosed ? {display: "none"} : null}>
                 <p className={navLink==="HOME" ? "fs-mid bx active" : "fs-mid bx unactive"} onClick={(e)=>setNavLink(e.target.innerText)}>HOME</p>
                 <p className={navLink==="ABOUT US" ? "fs-mid bx active" : "fs-mid bx unactive"} onClick={(e)=>setNavLink(e.target.innerText)}>ABOUT US</p>
                 <p className={navLink==="ADMINISTRATION" ? "fs-mid bx active" : "fs-mid bx unactive"} onClick={(e)=>setNavLink(e.target.innerText)}>ADMINISTRATION</p>
