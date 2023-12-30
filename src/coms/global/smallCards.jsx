d9261c8d83e697894f210f3db52174b62b5cfefb
AkshanshChauhan17 <akshanshchauhan47@gmail.com>
2023-12-30 15:44:38 +0530

    added lazy loading in msection

--- src/coms/global/smallCards.jsx
+++ src/coms/global/smallCards.jsx
@@ -1,7 +1,10 @@
+import { useState } from "react"
+
 export default function SmallCard({src, name, work_in}) {
+    const [isLoad, setIsLoad] = useState(true);
     return (
         <div className="small-card">
-            <img src={src} className="upper" />
+            <img src={src} className="upper" loading="lazy" onLoad={()=>setIsLoad(false)} style={isLoad ? {filter: "blur(10px)"} : {}}/>
             <div className="lower">
                 <p className="fs-mid bx">{name}</p>
                 <p className="fs-small">{work_in}</p>
